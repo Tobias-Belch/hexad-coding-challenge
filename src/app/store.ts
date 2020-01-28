e87b12cce40bdde074dee31033a3224c72cc0852
fea17e86 <fea17e86@mail.de>
2020-01-28 18:31:25 +0100

    adds store

---
+++ src/app/store.ts
@@ -0,0 +1,22 @@
+import { combineReducers, configureStore, Action } from "@reduxjs/toolkit";
+import { ThunkAction } from "redux-thunk";
+
+import foodsReducer from "../features/foods/foods.slice";
+import ratingsReducer from "../features/ratings/ratings.slice";
+
+const rootReducer = combineReducers({
+  foods: foodsReducer,
+  ratings: ratingsReducer
+});
+
+type RootState = ReturnType<typeof rootReducer>;
+
+const store = configureStore({
+  reducer: rootReducer
+});
+
+export type AppDispatch = typeof store.dispatch;
+
+export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
+
+export default store;
